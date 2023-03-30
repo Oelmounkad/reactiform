5f0b7702ecc3b39e6aca7daa0634090a784cba95
Oussama ELMOUNKAD <oelmounkad@gmail.com>
2023-03-30 15:47:29 +0200

    renaming App component file to TestingPlayground

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import TestingPlayground from "./App";
+import TestingPlayground from "./TestingPlayground";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
